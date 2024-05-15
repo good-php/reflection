45992046c2a469f99de4d5ca8e648e718b9af480
Alex Wells <autaut03@gmail.com>
2024-05-15 17:19:37 +0300

    fix: Cache namespace (#9)

--- src/ReflectorBuilder.php
+++ src/ReflectorBuilder.php
@@ -42,7 +42,7 @@ class ReflectorBuilder
 			new VerifiedCache(
 				new Psr16Cache(
 					new PhpFilesAdapter(
-						namespace: (string) InstalledVersions::getReference('thecodingmachine/graphqlite'),
+						namespace: (string) InstalledVersions::getReference('good-php/reflection'),
 						defaultLifetime: (int) $ttl?->format('s'),
 						directory: $path,
 					)
