8201a4c35f8c8a64c4412b097af66783bef69665
Alex Wells <autaut03@gmail.com>
2025-09-14 20:49:08 +0200

    docs: Add deployment

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -10,7 +10,7 @@ export default defineConfig({
 	integrations: [
 		starlight({
 			// Brand
-			title: 'Good PHP reflection',
+			title: 'Good PHP Reflection',
 			logo: {
 				src: './src/assets/logo.png',
 			},
